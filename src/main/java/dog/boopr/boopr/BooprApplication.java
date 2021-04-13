e11531d2197aa463d3be0e5807982415b8544285
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-04-13 09:38:15 -0500

    Initial boilerplate code

---
+++ src/main/java/dog/boopr/boopr/BooprApplication.java
@@ -0,0 +1,13 @@
+package dog.boopr.boopr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dog/boopr/boopr/BooprApplicationTests.java
@@ -0,0 +1,13 @@
+package dog.boopr.boopr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
