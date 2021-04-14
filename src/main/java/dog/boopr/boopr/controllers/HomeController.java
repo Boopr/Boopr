e25bbc6b583c556a6d83b3ce02bed92b1fc01c1d
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-04-14 10:08:54 -0500

    passed data to the home page

--- src/main/java/dog/boopr/boopr/controllers/HomeController.java
+++ src/main/java/dog/boopr/boopr/controllers/HomeController.java
@@ -1,11 +1,20 @@
 package dog.boopr.boopr.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import dog.boopr.boopr.models.Dog;
+import dog.boopr.boopr.repositories.DogRepository;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private DogRepository dogDao;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -14,6 +23,11 @@ public class HomeController {
 
     @GetMapping("/home")
     public String homePage(Model model) {
+
+        //we're pull from the dog repo
+        List<Dog> dogs = dogDao.findAll();
+        //and pushing to the frontend
+        model.addAttribute("dogs", dogs);
         return "/main";
     }
 
