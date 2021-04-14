58c0c65546a0d83f6ff3fb71ebcffdb714f3e2a8
Kole Morgan <kmorgan0057@gmail.com>
2021-04-14 11:27:23 -0500

    committing to merge

--- src/main/java/dog/boopr/boopr/repositories/ImageRepository.java
+++ src/main/java/dog/boopr/boopr/repositories/ImageRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import dog.boopr.boopr.models.Boop;
 import dog.boopr.boopr.models.Image;
 import dog.boopr.boopr.models.User;
 
