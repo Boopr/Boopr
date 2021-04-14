c7ac6310e8716d02b76eed7cff8902d4f9af262a
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-04-14 09:59:05 -0500

    Merge branch 'jacob' of github.com:Boopr/Boopr into joshua-talltree # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.
    created controller for main page.

--- src/main/java/dog/boopr/boopr/controllers/HomeController.java
+++ src/main/java/dog/boopr/boopr/controllers/HomeController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         return "index";
     }
 
     @GetMapping("/home")
-    public String homePage(Model model){
+    public String homePage(Model model) {
         return "/main";
     }
+
 }
