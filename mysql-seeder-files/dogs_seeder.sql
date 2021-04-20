3568829cdc86717f51241df06787e94acd222c29
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-04-20 15:10:28 -0500

    feat: created seeders for both the dogs and the users.

---
+++ mysql-seeder-files/dogs_seeder.sql
@@ -0,0 +1,17 @@
+USE boopr;
+
+
+INSERT INTO dogs (id, bio, lat, lon, name, sex, owner_id)
+VALUES (19, 'I am a handful because I love to chase everything', 29.445387, -99.335464, 'Boss', 1, 13 ),
+       (20, 'I am a peaceful dog that loves alone time and sunbathing', 29.445386, -99.335464, 'Candy', 0, 13 ),
+       (21, 'My favorite thing to do is tear up paper and chew on the carpet', 32.647734, -96.487647, 'Max', 1, 14 ),
+       (22, 'I love be around other dogs, my size or bigger, we have fun most of the time', 32.58493, -96.33222, 'Sasquach', 1, 15),
+       (23, 'I am gentle and loyal, protecting my Mom and Dad is my full-time job', 32.782780, -96.610001, 'Gina', 0, 16),
+       (24, 'I love to cuddle and I wake my Mom us every morning because I love her attention', 29.4241, 98.4936, 'Dennis', 1, 17),
+       (25, 'My life revolves around my mate, Sandy, I am always with her', 32.782780, -96.610001, 'Bane', 1, 18 ),
+       (26, 'If I go any where, I need my mate, Bane, with me, he is my life', 32.782780, -96.610001, 'Sandy', 0, 18 ),
+       (27, 'I am like a the energizer bunny, I never stop', 29.4241, 98.4936, 'Skippy', 0, 19),
+       (28, 'I sleep most of the day because I am an older, very calm dog', 32.58493, -96.33222, 'Shiloh', 1, 20),
+       (29, 'I am either eating, sleeping, or licking somebody, I am the friendliest dog around', 32.58493, -96.33222, 'Sasha', 0, 20),
+       (30, 'I am very mild-mannered until you get me started, after that, it is hard to slow me down', 29.4241, 98.4936, 'Sonny', 1, 21),
+       (31, 'I am the most mischievous dog around, I get into everything I see', 29.4241, 98.4936, 'Dasher', 0, 21)
---
+++ mysql-seeder-files/users_seeder.sql
@@ -0,0 +1,13 @@
+USE boopr;
+
+
+INSERT INTO users (id, email, password, username)
+VALUES (13, 'john23@gmail.com', '$2a$11$WXDXjZHtia.FezcIbXMgc.vW/KuQJN1e2ym1Js056O4vQ80T72pva', 'dogdad1'),
+       (14, 'masterdogman2@yahoo.com', '$2a$11$GJweidwAQIVjLJmAmstlhufqxo/hILvFVOori8MXrL1atuf4AvR0.', 'mananddog1'),
+       (15, 'dogdays3@hotmail.com', '$2a$11$fWMqeTki.wTPWXmcoWA14.PEmvKUz.h/YJZr93KrU0uOb4yTXCIjG', 'numer1dogmom22'),
+       (16, 'caninelady55@dognet.com', '$2a$11$XA0qZN/wmlWOFnsbNGwC/OwBYVuWL9yOotqsVuTlh6.IK1axdSKyu', 'superdogmom12'),
+       (17, 'shepherddogdad@gmail.com', '$2a$11$efrU9.5uT.LgbG8Vu4Tv5OHGYQ//BTKC8rHPWEuJ0RWnDmTS/ci3m', 'Bigbreader1$'),
+       (18, 'dogfather58@yahoo.com', '$2a$11$DE5xuPIhCJWkKy4bBKvomuhtrIutG2krn7EygoSE/.SG.F8lqi9oy', 'rescueman12'),
+       (19, 'dogorama23@hotmail.com', '$2a$11$S6LUoaifijceGg6vqwRc6eus/mlIpu1917WQ3rqa2x.c/bjDGCJuW', 'alldogsgotoheaven1'),
+       (20, 'onlycanines2@gmail.com', '$2a$11$6ylPNPg5/om4FkfIOqv9WedA1k7d65rJkIGIGHLl4I2SWq4oaPtai', 'hounds4me23'),
+       (21, 'doggyparadise33@yahoo.com', '$2a$11$PX8ccHgkuJmkDvcMA8KT9./z5k5a2DvnArRVX2bJAEKWG87sCrqzW', 'Iamdogman44')
