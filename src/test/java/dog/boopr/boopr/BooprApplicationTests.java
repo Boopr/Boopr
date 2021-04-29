55f1be40701c70e6b96db9cd122a4146a438d7cb
kmorgport <kmorgan0057@gmail.com>
2021-04-29 11:19:29 -0500

    unit testing implementation

--- src/test/java/dog/boopr/boopr/BooprApplicationTests.java
+++ src/test/java/dog/boopr/boopr/BooprApplicationTests.java
@@ -1,13 +1,44 @@
 package dog.boopr.boopr;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootTest
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import dog.boopr.boopr.models.Breed;
+import dog.boopr.boopr.models.Dog;
+import dog.boopr.boopr.repositories.DogRepository;
+import dog.boopr.boopr.repositories.UserRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BooprApplication.class)
+@AutoConfigureMockMvc
 class BooprApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Autowired
+    private DogRepository dogDao;
+
+    @Autowired
+    private UserRepository userDao;
+
+	private Dog expectedDog;
+	private List<Breed> expectedBreeds;
+	private Breed expectedBreed;
+
+	@Before
+	public void setUp(){
+		this.expectedBreeds = new ArrayList<>();
+		this.expectedBreed = new Breed("Pitbull");
+		expectedBreeds.add(expectedBreed);
+		this.expectedDog = new Dog("test",expectedBreeds,true,"I'm a testy boi");
+	}
+
+	
 
 }
