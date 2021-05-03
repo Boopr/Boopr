bd98ac5fe35ada1f9915af3f6366593afc9dcb0f
kmorgport <kmorgan0057@gmail.com>
2021-05-03 14:00:30 -0500

    about us, landing page edits

--- src/test/java/dog/boopr/boopr/BooprModelTests.java
+++ src/test/java/dog/boopr/boopr/BooprModelTests.java
@@ -4,8 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/dog/boopr/boopr/DogRepositoryTest.java
+++ src/test/java/dog/boopr/boopr/DogRepositoryTest.java
@@ -1,43 +1,39 @@
-package dog.boopr.boopr;
-import org.junit.Assert;
+// package dog.boopr.boopr;
 
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.Assert.assertSame;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+// import org.junit.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import dog.boopr.boopr.models.Breed;
-import dog.boopr.boopr.models.Dog;
-import dog.boopr.boopr.repositories.DogRepository;
+// import dog.boopr.boopr.models.Breed;
+// import dog.boopr.boopr.models.Dog;
+// import dog.boopr.boopr.repositories.DogRepository;
 
-@DataJpaTest
-public class DogRepositoryTest {
+// @DataJpaTest
+// public class DogRepositoryTest {
     
-    @Autowired
-    private DogRepository dogTest;
+//     @Autowired
+//     private DogRepository dogTest;
 
-    @Test
-    public void isDaoPullSame(){
-        // //give
-        List<Breed> breedlist = new ArrayList<>();
-        breedlist.add(new Breed("Pitbull"));
-        Dog pup = new Dog("Pickles",
-        breedlist,
-        false,
-        "I'm a crunchy boi");
-        // Object clonnedPup = pup;
-        dogTest.save(pup);
-        // //when
-        // boolean exists = underTest.selectExistsName("Pickles");
-        //then
-        assertSame(pup,dogTest.findDogByName("Pickles").get(0));
+//     @Test
+//     public void isDaoPullSame(){
+//         // //give
+//         List<Breed> breedlist = new ArrayList<>();
+//         breedlist.add(new Breed("Pitbull"));
+//         Dog pup = new Dog("Pickles",
+//         breedlist,
+//         false,
+//         "I'm a crunchy boi");
+//         // Object clonnedPup = pup;
+//         dogTest.save(pup);
+//         // //when
+//         // boolean exists = underTest.selectExistsName("Pickles");
+//         //then
+//         assertSame(pup,dogTest.findDogByName("Pickles").get(0));
 
-    }
-}
+//     }
+// }
