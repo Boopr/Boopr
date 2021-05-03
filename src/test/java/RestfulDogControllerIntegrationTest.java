c80b07bf6a248ac6106c698c76f186860abc1a1c
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-05-03 14:59:53 -0500

    changes to icons

--- src/test/java/RestfulDogControllerIntegrationTest.java
+++ src/test/java/RestfulDogControllerIntegrationTest.java
@@ -1,26 +1,26 @@
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.TestPropertySource;
+// import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.web.servlet.MockMvc;
 
-import dog.boopr.boopr.BooprApplication;
-import dog.boopr.boopr.repositories.DogRepository;
+// import dog.boopr.boopr.BooprApplication;
+// import dog.boopr.boopr.repositories.DogRepository;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = BooprApplication.class)
-@AutoConfigureMockMvc
-@TestPropertySource(
-  locations = "classpath:application-integrationtest.properties")
-public class RestfulDogControllerIntegrationTest{
+// @RunWith(SpringRunner.class)
+// @SpringBootTest(classes = BooprApplication.class)
+// @AutoConfigureMockMvc
+// @TestPropertySource(
+//   locations = "classpath:application-integrationtest.properties")
+// public class RestfulDogControllerIntegrationTest{
 
-    @Autowired
-    private MockMvc mvc;
+//     @Autowired
+//     private MockMvc mvc;
 
-    @Autowired
-    private DogRepository dogDao;
+//     @Autowired
+//     private DogRepository dogDao;
 
-    // write test cases here
-}
+//     // write test cases here
+// }
