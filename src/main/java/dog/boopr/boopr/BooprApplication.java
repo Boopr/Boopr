381cd1f1bb671572f6b38ec157db0fb9a1420f92
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-05-07 03:11:04 -0500

    bean to size images

--- src/main/java/dog/boopr/boopr/BooprApplication.java
+++ src/main/java/dog/boopr/boopr/BooprApplication.java
@@ -1,9 +1,12 @@
 package dog.boopr.boopr;
 
+import javax.servlet.MultipartConfigElement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class BooprApplication {
@@ -12,6 +15,14 @@ public class BooprApplication {
 		SpringApplication.run(BooprApplication.class, args);
 	}
 
-	
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		DataSize size = DataSize.ofMegabytes(10);
+
+		factory.setMaxFileSize(size);
+		factory.setMaxRequestSize(size);
+		return factory.createMultipartConfig();
+	}
 
 }
