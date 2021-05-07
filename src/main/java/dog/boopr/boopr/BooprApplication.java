f3b49933bd091e8cf2e42371cd265e9452b06830
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-05-07 02:47:22 -0500

    Image upload yml

--- src/main/java/dog/boopr/boopr/BooprApplication.java
+++ src/main/java/dog/boopr/boopr/BooprApplication.java
@@ -1,7 +1,9 @@
 package dog.boopr.boopr;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class BooprApplication {
@@ -9,5 +11,7 @@ public class BooprApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BooprApplication.class, args);
 	}
+
+	
 
 }
--- src/main/java/dog/boopr/boopr/configs/ImageConfig.java
+++ src/main/java/dog/boopr/boopr/configs/ImageConfig.java
@@ -17,4 +17,5 @@ public class ImageConfig implements WebMvcConfigurer{
                 .addResourceLocations("file:" + uploadPath);
     }
 
+  
 }
